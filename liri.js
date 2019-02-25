9c9944e37d1ea5633326a982ba4621afd190698b
tochwat <tochwat@gmail.com>
2019-02-24 18:02:02 -0800

    spotify feature working

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+var axios = require("axios");
 require('dotenv').config();
 var Spotify = require('node-spotify-api');
 
@@ -8,29 +9,40 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
-});
-
-
-
-
-
-
 var command = process.argv[2];
-var media = process.argv[3];
+
+var nodeArgs = process.argv;
+var media = "";
+for (var i=3; i<nodeArgs.length; i++) {
+  if (i>3 && i<nodeArgs.length){
+    media = media + "+" + nodeArgs[i];
+  } else {
+    media += nodeArgs[i];
+  }
+}
 
 
 if (command === "spotify-this-song") {
+    spotify.search({ type: 'track', query: media, limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+    
+    // console.log(data.tracks.items[0]); 
 
-  
+    console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+    console.log("Song title: " + data.tracks.items[0].name); 
+    console.log("Preview link: " + data.tracks.items[0].external_urls.spotify); 
+    console.log("Album: " + data.tracks.items[0].album.name); 
 
+    });
+} else if (command === "movie-this") {
+  var queryUrl = "http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy";
 
-
-
+  axios.get(queryUrl).then(
+    function(response) {
+      console.log("Release Year: " + response.data.Year);
+      console.log(response.data);
+    }
+  );
 }
